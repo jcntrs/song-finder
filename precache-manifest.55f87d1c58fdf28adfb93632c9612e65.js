2868d24233d35f75cf8c8cd39d4f19d6227e212a
Jean C. Contreras <jean.cntrs@gmail.com>
2020-03-30 03:41:37 -0300

    Updates

---
+++ precache-manifest.55f87d1c58fdf28adfb93632c9612e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4660f0e474335498e5ae4a1efac928",
+    "url": "/song-finder/index.html"
+  },
+  {
+    "revision": "594491625b2558687881",
+    "url": "/song-finder/static/js/2.3f9ef58c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/song-finder/static/js/2.3f9ef58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455e6b1575e0abab7997",
+    "url": "/song-finder/static/js/main.b9c11954.chunk.js"
+  },
+  {
+    "revision": "3af188394fd97b02af38",
+    "url": "/song-finder/static/js/runtime-main.3811fcb5.js"
+  }
+]);
